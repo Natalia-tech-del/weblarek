5b0ee730219802a70315b330ab75611a5be8c13a
Natalia Kirilyuk <kirilyukn1986@yandex.ru>
2025-12-03 15:38:36 +0300

    добавлен класс коммуникации

---
+++ src/components/communication/WebLarekApi.ts
@@ -0,0 +1,19 @@
+import { IApi, IOrder, IResultOrder, IProduct, IProductResponse} from "../../types";
+import { Api } from "../base/Api";
+
+export class WebLarekApi{
+    private api: IApi;
+
+    constructor(api: IApi) {
+        this.api = api;
+    }
+
+    async getProducts(): Promise<IProduct[]> {
+        const response = await this.api.get<IProductResponse>('/product/');
+        return response.items;
+    }
+
+    async postOrder(data: IOrder): Promise<IResultOrder> {
+        return await this.api.post<IResultOrder>('/order/', data);
+    }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -8,24 +8,46 @@ export interface IApi {
 export type TPayment = 'card' | 'cash' | '';
 
 export interface IProduct {
-  id: string;           // уникальный идентификатор товара
-  description: string;  // подробное описание товара
-  image: string;        // путь к изображению для карточки товара
-  title: string;        // название товара
-  category: string;     // группа, к которой относится товар
-  price: number | null; // цена товара
+    id: string;           // уникальный идентификатор товара
+    description: string;  // подробное описание товара
+    image: string;        // путь к изображению для карточки товара
+    title: string;        // название товара
+    category: string;     // группа, к которой относится товар
+    price: number | null; // цена товара
 }
 
 export interface IBuyer {
-  payment: TPayment;   // способ оплаты
-  email: string;       // электронная почта покупателя
-  phone: string;       // телефон покупателя
-  address: string;     // адрес доставки
+    payment: TPayment;   // способ оплаты
+    email: string;       // электронная почта покупателя
+    phone: string;       // телефон покупателя
+    address: string;     // адрес доставки
 }
 
-export interface ValidationErrors {
-  payment?: string;     // ошибка для поля: способ оплаты
-  email?: string;       // ошибка для поля: электронная почта покупателя
-  phone?: string;       // ошибка для поля: телефон покупателя
-  address?: string;     // ошибка для поля: адрес доставки
+export interface IProductResponse {
+    total: number;       // количество товаров в каталоге
+    items: IProduct[];     // массив товаров в каталоге
 }
+
+export interface IOrder extends IBuyer {
+    total: number;       // общая сумма заказа
+    items: string[];     // массив идентификаторов заказанных товаров
+}
+
+export interface IOrderSuccess {
+    id: string;          // id заказа
+    total: number;       // общая стоимость заказа от сервера
+}
+
+export interface IOrderError {
+    error: string;       // ошибка
+}
+
+export type IResultOrder = IOrderSuccess | IOrderError;
+
+export interface ValidationErrors {
+    payment?: string;     // ошибка для поля: способ оплаты
+    email?: string;       // ошибка для поля: электронная почта покупателя
+    phone?: string;       // ошибка для поля: телефон покупателя
+    address?: string;     // ошибка для поля: адрес доставки
+}
+
