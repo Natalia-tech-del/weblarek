ab616e3c6030513c89c7cca33c3578a896f98aea
Natalia Kirilyuk <kirilyukn1986@yandex.ru>
2025-12-24 11:22:57 +0300

    Создан компонент Modal

---
+++ src/components/view/Modal.ts
@@ -0,0 +1,31 @@
+import { ensureElement } from "../../utils/utils";
+import { Component } from "../base/Component";
+import { IEvents } from "../base/Events";
+
+interface IModal {
+  content: HTMLElement;
+}
+
+export class Modal extends Component<IModal> {
+  protected contentElement: HTMLElement;
+  protected closeButton: HTMLButtonElement;
+
+  constructor(protected events: IEvents, container: HTMLElement) {
+    super(container);
+
+    this.contentElement = ensureElement<HTMLElement>('.modal__content', this.container);
+    this.closeButton = ensureElement<HTMLButtonElement>('.modal__close', this.container);
+
+    this.closeButton.addEventListener('click', () => {
+      this.events.emit('modal:close');
+    });
+  }
+
+  set content(element: HTMLElement) {
+    if (!element) {
+      this.contentElement.replaceChildren();
+    } else {
+    this.contentElement.replaceChildren(element);
+    }
+  }
+}
