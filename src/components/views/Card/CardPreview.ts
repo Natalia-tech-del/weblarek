8f14f9a9a5955c5c436ed248c0c3b1ae4b0f2365
Natalia Kirilyuk <kirilyukn1986@yandex.ru>
2025-12-26 14:02:19 +0300

    Доработан класс CardPreview

--- src/components/views/Card/CardPreview.ts
+++ src/components/views/Card/CardPreview.ts
@@ -1,7 +1,8 @@
 import { categoryMap } from "../../../utils/constants";
 import { ensureElement } from "../../../utils/utils";
 import { TCard, Card } from "./Card";
-import { IProduct, CategoryKey } from "../../../types";
+import { IProduct, CategoryKey, ICardActions } from "../../../types";
+
 
 export type TCardPreview = TCard & 
   Pick<IProduct, 'category' | 'description'> & {
@@ -9,5 +10,44 @@ export type TCardPreview = TCard &
     buttonText: string};
 
 export class CardPreview extends Card<TCardPreview> {
+  protected imageElement: HTMLImageElement;
+  protected categoryElement: HTMLElement;
+  protected textElement: HTMLElement;
+  protected cardButton: HTMLButtonElement;
 
+  constructor(container: HTMLElement, actions?: ICardActions) {
+    super(container);
+    this.categoryElement = ensureElement<HTMLElement>('.card__category',this.container);
+
+    this.imageElement = ensureElement<HTMLImageElement>('.card__image',this.container);
+
+    this.textElement = ensureElement<HTMLElement>('.card__text', this.container);
+
+    this.cardButton = ensureElement<HTMLButtonElement>('.card__button', this.container);
+
+    
+     if (actions?.onClick) {
+      this.cardButton.addEventListener('click', actions.onClick);
+    }
+  }
+
+  set category(value: string) {
+    this.categoryElement.textContent = value;
+
+    for (const key in categoryMap) {
+      this.categoryElement.classList.toggle(categoryMap[key as CategoryKey], key === value);
+    }
+  }
+
+  set image(data: {src: string, alt: string}) {
+    this.setImage(this.imageElement, data.src, data.alt);
+  }
+  
+  set description(value: string) {
+    this.textElement.textContent = value;
+  }
+
+  set buttonText(value: string) {
+    this.cardButton.textContent = value;
+  }
 }
