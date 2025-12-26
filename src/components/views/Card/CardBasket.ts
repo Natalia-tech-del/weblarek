3b44314e8915e168de51d551d439b5f3736299fd
Natalia Kirilyuk <kirilyukn1986@yandex.ru>
2025-12-26 16:47:27 +0300

    Создан класс CardBasket

---
+++ src/components/views/Card/CardBasket.ts
@@ -0,0 +1,25 @@
+import { ensureElement } from "../../../utils/utils";
+import { TCard, Card } from "./Card";
+import { ICardActions } from "../../../types";
+
+export type TCardBasket = TCard & { index: number};
+
+export class CardBasket extends Card<TCardBasket> {
+  protected itemIndexElement: HTMLElement;
+  protected basketDeleteButton: HTMLButtonElement;
+
+  constructor(container: HTMLElement, actions?: ICardActions) {
+    super(container);
+    this.itemIndexElement = ensureElement<HTMLElement>('.basket__item-index',this.container);
+
+    this.basketDeleteButton = ensureElement<HTMLButtonElement>('.basket__item-delete', this.container);
+    
+   if (actions?.onClick) {
+      this.basketDeleteButton.addEventListener('click', actions.onClick);
+    }
+  }
+
+  set index(value: number) {
+    this.itemIndexElement.textContent = String(value);
+  }
+}
