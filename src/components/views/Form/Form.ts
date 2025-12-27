00048437dcee90b313ac61dc6645eb957ed70abb
Natalia Kirilyuk <kirilyukn1986@yandex.ru>
2025-12-27 19:27:13 +0300

    Доработан родительский класс Form

--- src/components/views/Form/Form.ts
+++ src/components/views/Form/Form.ts
@@ -9,12 +9,14 @@ export interface IForm {
 export abstract class Form<T extends IForm> extends Component<T> {
   protected formSubmitButton: HTMLButtonElement;
   protected errorsFormElement: HTMLElement;
+  protected formElement: HTMLFormElement;
 
   constructor(container: HTMLElement) {
     super(container);
-
-    this.formSubmitButton = ensureElement<HTMLButtonElement>('button[type="submit"]', this.container);
-    this.errorsFormElement = ensureElement<HTMLElement>('.form__errors', this.container);
+    
+    this.formElement = this.container as HTMLFormElement;
+    this.formSubmitButton = ensureElement<HTMLButtonElement>('button[type="submit"]', this.formElement);
+    this.errorsFormElement = ensureElement<HTMLElement>('.form__errors', this.formElement);
   }
 
   set errors(value: string) {
